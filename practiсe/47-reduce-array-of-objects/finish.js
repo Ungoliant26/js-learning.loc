cc1d86e99cd7792c2d91a46e018ac6f7f6dc0d70
Kirill Royako <rakot666111@gmail.com>
2024-01-23 17:58:59 +0300

    main: tasks hard array reduce

--- practiсe/47-reduce-array-of-objects/finish.js
+++ practiсe/47-reduce-array-of-objects/finish.js
@@ -6,6 +6,39 @@
  * у которых количество комментариев не меньше "minimalComentsQty"
  */
 
+/**
+ * Создаём функцию popularPostsIds с двумя параметрами posts и minimalComentsQty
+ * reduce содержит 2 аргумента
+ *
+ * 1 аргумент - callback функция с двумя параметрами (themePostsIds, themePost)
+ * themePostsIds - аккумулирует значения путем передачи от одного вызова функции к другому
+ * themePost - элемент массива posts
+ * В теле функции возвращается обновлённое значение для параметра themePostsIds
+ * С помощью тернарного оператора неявно возвращем результат выражения
+ * В условии оператора проверяем кол-во комментариев в определённом посте
+ * Метод concat берёт 2 массива, соединяет и возвращает новый
+ * Новый массив будет новым значением для переменной themePostsIds
+ * Если условие ложно, то возвращаем предыдущее значение themePostsIds
+ *
+ * 2 аргумент - начальное значение themePostsIds
+ * значение будет передаваться следующему вызову callback функции
+ * @param {Array} posts
+ * @param {Number} minimalComentsQty
+ * @returns результат метода reduce
+ */
+
+function popularPostsIds(posts, minimalComentsQty) {
+  return posts.reduce(
+    //Первый аргумент метода массивов reduce
+    (themePostsIds, themePost) =>
+      themePost.comments >= minimalComentsQty
+        ? themePostsIds.concat([themePost.postId])
+        : themePostsIds,
+    //Второй аргумент
+    []
+  )
+}
+
 const inputPosts = [
   {
     title: 'Как быстро выучить JavaScript?',
