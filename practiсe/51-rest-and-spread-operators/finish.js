6840f64e846c9248b0e0ea777907d05bdfa028e4
Kirill Royako <rakot666111@gmail.com>
2024-01-26 19:13:12 +0300

    main: tasks rest, spread, default

--- practiсe/51-rest-and-spread-operators/finish.js
+++ practiсe/51-rest-and-spread-operators/finish.js
@@ -13,21 +13,36 @@
  * так и оператор "spread".
  */
 
+const meanScore = (...midArray) => {
+  // ... rest оператор
+  if (midArray.some((mid) => typeof mid !== 'number')) {
+    console.error('Все аргументы в вызове функции должны быть числами!')
+    return
+  }
+
+  /**
+   * userNumber1 / midArray.length + userNumber2 / midArray.length + ... + userNumber10 / midArray.length
+   * Равносильно
+   * userNumber1 + userNumber2 + ... + userNumber10 / midArray.length
+   */
+  return midArray
+    .reduce((mid, userNumber) => mid + userNumber / midArray.length, 0)
+    .toFixed(2)
+}
+
 const scores1 = [0, 1.5, 2.5, 3.7]
 const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
 const scores3 = [1.3, 2.5, 1.9]
 const scores4 = ['abc', 1.3, true, 2.5, 1.9]
 
-console.log(meanScore(/* все элементы из массива "scores1" */))
+console.log(meanScore(...scores1)) // ... spread оператор
 // 1.93
 
-console.log(meanScore(/* все элементы из массивов "scores1" и "scores2" */))
+console.log(meanScore(...scores1, ...scores2))
 // 2.8
 
-console.log(
-  meanScore(/* все элементы из массивов "scores1", "scores2" и "scores3" */)
-)
+console.log(meanScore(...scores1, ...scores2, ...scores3))
 // 2.59
 
-console.log(meanScore(/* все элементы из массива "scores4" */))
+console.log(meanScore(...scores4))
 // Все аргументы в вызове функции должны быть числами!
